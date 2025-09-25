bac0d7271788fa926e5081ad88cd98faf04275dd
Roberto Carlos Oviedo Rodriguez <64382leo@universidaddeleon.edu.mx>
2025-09-24 18:33:21 -0600

    version 1 feacture/roberto

--- main.py
+++ main.py
@@ -6,7 +6,17 @@ def main():
     op.sumar()
     op.mostrarResultado()
     
-    #Realiza las pruebas con las nuevas operaciones
+    op.restar()
+    op.mostrarResultado()
+
+    op.multiplicar()
+    op.mostrarResultado()
+
+    op.dividir()
+    op.mostrarResultado()
+
+    op.modulo()
+    op.mostrarResultado()
     
 
 if __name__ == "__main__":
